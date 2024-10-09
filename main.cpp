573a561c8b12afb77218cc0d417aa3ea1c58cedf
MaciekMaryna <mmarynowski@sii.pl>
2024-10-09 20:26:47 +0200

    Big grup changes

--- main.cpp
+++ main.cpp
@@ -6,18 +6,20 @@
  ******************************************************************************/
 
 #include <iostream>
+#include <iomanip>
 #include <chrono>
-//#include <string>
 #include <stdint.h>
 
 /******************************************************************************
  * Defines and macros
  ******************************************************************************/
 
+
 /******************************************************************************
  * Typedefs
  ******************************************************************************/
-typedef enum 
+/*
+enum 
 {
     PLN = 0,
     EUR,
@@ -27,8 +29,43 @@ typedef enum
     RUP,
     SEK,
     HOK,
-    HUF
-} Currencies_t;
+    HUF,
+    NUMBER_OF_CURRENCIES
+};
+*/
+
+enum Status_t
+{
+    OPERATION_OK = 0,
+    COMMON_FAULT,
+    NO_ANOUGH_FUNDS,
+    NO_ANOUGH_DEBIT,
+    NO_ANOUGH_CURRENCY,
+    CANNOT_SET_SUCH_BALANCE,
+    INVALID_CURRENCY_NAME,
+    INVALID_IBAN_NUMBER,
+
+    NUMBER_OF_STATUSES
+};
+
+std::string ErrorNames[] =
+{
+    "OPERATION_OK",
+    "COMMON_FAULT",
+    "NO_ANOUGH_FUNDS",
+    "NO_ANOUGH_DEBIT",
+    "NO_ANOUGH_CURRENCY",
+    "CANNOT_SET_SUCH_BALANCE",
+    "INVALID_CURRENCY_NAME",
+    "INVALID_IBAN_NUMBER"
+};
+
+/******************************************************************************
+ * Global variables
+ ******************************************************************************/
+uint32_t OperationNumber;
+std::string DomesticCurrency = "PLN";
+
 /******************************************************************************
  * Classes
  ******************************************************************************/
@@ -36,60 +73,131 @@ typedef enum
 class Account
 {
     private:
-    protected:
-    public:
-        uint64_t IBAN;
-        Currencies_t DomesticCurrency;
-        double Balance;
-        void Deposit(const double);
-        void Withdraw(const double); 
-        double GetBalance(void);
-        void operator+=(double);
-        void operator-=(double);
-        friend std::ostream & operator<<(std::ostream & console, const Account &obj) // *** dlaczego nie mogę definicji wynieść poza klasę?
-        {
-            console << obj.Balance;
-            return console;
-        }
 
+    protected:
+        uint64_t IBAN;
+        double Balance;
+        double BalanceOld = 0;   
+        Status_t LastOperStatus = OPERATION_OK;        
+        std::string Currency = "---";        //czy wartości początkowe można nadać wyłącznie konstruktorem lub seterem, a nie w ten sposób?
+    public:
+        void Set_IBAN(const uint64_t);
+        uint64_t Get_IBAN(void);
+        void Set_Balance(const double);
+        double Get_Balance(void);        
+        double Get_BalanceOld(void);
+        Status_t Get_LastOperStatus(void);
+        std::string Get_Currency(void);
+        void Set_Currency(std::string);        
+        void Deposit(const double);        
+        virtual void Withdraw(const double) = 0;         
+        void operator+=(const double);
+        void operator-=(const double);
+        friend std::ostream & operator<<(std::ostream &, const Account &); 
         Account()
         {
-            this->IBAN = (uint64_t)0;
-            this->DomesticCurrency = PLN;
-            this->Balance = (double)0;            
+            this -> IBAN = (uint64_t)0;
+            this -> Currency = DomesticCurrency;
+            this -> Balance = (double)0;            
         }
 
-        Account(uint64_t iban, double bal): IBAN(iban), Balance(bal)
+        Account(uint64_t iban, double bal, std::string cur)
         {
-            this->IBAN = (uint64_t)0;
-            this->DomesticCurrency = PLN;
-            this->Balance = (double)0;            
+            this -> IBAN = iban;
+            this -> Currency = cur;
+            this -> Balance = bal;            
         }
 };
 
+std::ostream & operator<<(std::ostream & console, const Account &obj)
+{
+    console << obj.Balance;
+    return console;
+}
+
 void Account::Deposit(const double dep)
 {
+    this -> BalanceOld = this -> Balance;
+    this -> Balance += dep;
+    this -> LastOperStatus = OPERATION_OK;
+}
+
+void Account::Set_IBAN(const uint64_t iban)
+{
+    if (0 != iban)
+    {
+        this -> IBAN = iban;
+        this -> LastOperStatus = OPERATION_OK;
+    }
+    else
+    {
+        this -> LastOperStatus = INVALID_IBAN_NUMBER;       
+    }
+}
+
+uint64_t Account::Get_IBAN(void)
+{
+    return this -> IBAN;
+}
+
+void Account::Set_Balance(const double bal)
+{
+    this -> BalanceOld = this -> Balance;
+    this -> Balance = bal;
+}
+
+double Account::Get_Balance(void)
+{
+    return this -> Balance;
+}
+
+double Account::Get_BalanceOld(void)
+{
+    return this -> BalanceOld;
+}
+
+std::string Account::Get_Currency(void)
+{
+    std::string result;
+    if (3 != this -> Currency.length())
+    {
+        result = "---\0";
+    }
+    else
+    {
+        result = this -> Currency;
+    }
+    return result;
+}
+
+void Account::Set_Currency(std::string curr)
+{
+    if (3 != curr.length())
+    {
+        this -> LastOperStatus = INVALID_CURRENCY_NAME;
+    }
+    else
+    {
+        this -> Currency = curr;
+        this -> LastOperStatus = OPERATION_OK;
+    }
+}
+
+Status_t Account::Get_LastOperStatus(void)
+{
+    return this -> LastOperStatus;
+}
+
+void Account::operator+=(const double dep)
+{
+    this -> BalanceOld = this -> Balance;
     this -> Balance += dep;
 }
 
-void Account::Withdraw(const double wit)
+void Account::operator-=(const double wit)
 {
-        this -> Balance -= wit;
-}
-
-double Account::GetBalance(void)
-{
-    return this->Balance;
-}
-
-void Account::operator+=(double dep)
-{
-    this->Balance += dep;
-}
-
-void Account::operator-=(double wit)
-{
-    this->Balance -= wit;
+    this -> BalanceOld = this -> Balance;
+    this -> Balance -= wit;
 }
 
 /*************** Class CheckingAccout  ***************/
@@ -98,9 +206,25 @@ class CheckingAccout : public Account
     private:
     protected:
     public:
+        void PayOverdraftInterest(void);    
+        void Withdraw(const double) override;      
 };
 
-/*********** Class PersonaAccount (debit) ************/
+void CheckingAccout::Withdraw(const double wit)
+{
+    if(Balance - wit < 0)
+    {
+        this -> LastOperStatus = NO_ANOUGH_FUNDS;
+    }
+    else 
+    {
+        this -> BalanceOld = this -> Balance;
+        this -> Balance -= wit;
+        this -> LastOperStatus = OPERATION_OK;
+    }
+}
+
+/*********** Class PersonalAccount (debit) ************/
 class PersonalAccount : public Account
 {
     private:
@@ -109,16 +233,31 @@ class PersonalAccount : public Account
         double OverdraftLimit; // should be a positive number
         float OverdraftInterestRate;
         void PayOverdraftInterest(void);
+        void Withdraw(const double) override;
 };
+
+void PersonalAccount::Withdraw(const double wit)
+{
+    if(Balance - wit < (-OverdraftLimit))
+    {
+        this -> LastOperStatus = NO_ANOUGH_DEBIT;        
+    }
+    else 
+    {
+        this -> BalanceOld = this -> Balance;
+        this -> Balance -= wit;
+        this -> LastOperStatus = OPERATION_OK; 
+    }
+} 
 
 void PersonalAccount::PayOverdraftInterest(void)
 {
     if (this->Balance < 0)
     {
-        this->Balance *= (1 + this->OverdraftInterestRate);
+        this -> BalanceOld = this -> Balance;
+        this -> Balance *= (1 + this->OverdraftInterestRate);
     }
 }
-
 
 /*************** Class SavingsAccout  ***************/
 class SavingsAccount : public Account
@@ -128,11 +267,27 @@ class SavingsAccount : public Account
     public:
         float InterestRate;
         void CapitaliseIterest(void);
+        void Withdraw(const double) override;           
 };
+
+void SavingsAccount::Withdraw(const double wit)
+{
+    if(Balance - wit < 0)
+    {
+        this -> LastOperStatus = NO_ANOUGH_FUNDS;        
+    }
+    else 
+    {   
+        this -> BalanceOld = this -> Balance;
+        this -> Balance -= wit;
+        this -> LastOperStatus = OPERATION_OK;        
+    }
+} 
 
 void SavingsAccount::CapitaliseIterest(void)
 {
-    this->Balance *= (1 + this->InterestRate);
+    this -> BalanceOld = this -> Balance;
+    this -> Balance *= (1 + this->InterestRate);
 }
 
 /*********** Class ForeginCurrencyAccout  ***********/
@@ -141,73 +296,123 @@ class ForeginCurrencyAccout : public Account
     private:
     protected:
     public:
-        Currencies_t ForreignCurrency;
+        void Withdraw(const double) override;               
 };
 
-
-
-/******************************************************************************
- * Non class-member functions
- ******************************************************************************/
-/*
-std::ostream & operator<<(std::ostream & Os, const Person & Prs) 
+void ForeginCurrencyAccout::Withdraw(const double wit)
 {
-    Os  << std::endl << "\t\tFORENAME:     " << Prs.Forename
-        << std::endl << "\t\tSURNAME:    " << Prs.Surname 
-        << std::endl << "\t\tHOME ADDRESS : " << Prs.HomeAddress
-        << std::endl << "\t\tBIRTH DATE :   " << Prs.BirthDate;
-    return Os;
+    if(Balance - wit < 0)
+    {
+        this -> LastOperStatus = NO_ANOUGH_FUNDS;
+    }
+    else 
+    {
+        this -> BalanceOld = this -> Balance;
+        this -> Balance -= wit;
+        this -> LastOperStatus = OPERATION_OK;
+    }
+} 
+/*******************************************************************************
+* Common non class-member functions
+*******************************************************************************/
+void ShowSeparationLine(void)
+{
+    std::cout << "+-----+----------------------------+------------------+------------------+--------------------------------+" << std::endl;    
 }
 
-std::ostream & operator<<(std::ostream & Os, const Account & Acc)
+void ShowColumnsNames(void)
 {
-        Os  << std::endl 
-        << std::endl << "ID NUMBER:        " << Acc.IdNum 
-        << std::endl << "DATE OF EXPIRY:   " << Acc.ExpiryDate 
-        << std::endl << "FUNDS :           " << Acc.Funds
-        << std::endl << "LAST OP. STATUS : " << Acc.LastOperationStatus       
-        << std::endl << "OWNER :           " << Acc.Owner;
-    return Os;
+    ShowSeparationLine();
+    std::cout << "| No. | IBAN number                | BalaceBefore     | BalanceAfter     | OperStatus                     |" << std::endl;
+    ShowSeparationLine();
 }
-*/
+
+void ShowLastOperationRaport(Account* Tab)
+{
+    std::cout <<    std::right << "| " <<
+                    std::setw(3) << std::setfill('0') << OperationNumber  << " | " <<
+                    std::setw(26) << std::setfill(' ') << Tab -> Get_IBAN() << " | " << 
+                    std::setw(12) << std::setfill(' ') << Tab -> Get_BalanceOld() << " " << 
+                    std::setw(3) << Tab -> Get_Currency() <<  " | " <<
+                    std::setw(12) << std::setfill(' ') << Tab -> Get_Balance()  <<  " " <<
+                    std::setw(3) << Tab -> Get_Currency() <<  " | " <<
+                    std::left << 
+                    std::setw(30) << std::setfill(' ') << ErrorNames[Tab -> Get_LastOperStatus()]  <<  " | " <<
+                    std::endl;
+}
 /*******************************************************************************
 * Main function
 *******************************************************************************/
 int main()
 {
+
+    ShowColumnsNames();
     CheckingAccout MyAccount1;
-    MyAccount1 += 1000;
-    MyAccount1 -= 5;
-    std::cout<<"Balance: "<< MyAccount1 <<std::endl;
+    MyAccount1.Set_IBAN(0);
+    PersonalAccount MyAccount2;    
+    SavingsAccount MyAccount3; 
+    ForeginCurrencyAccout MyAccount4;   
 
-    PersonalAccount MyAccount2;
-    MyAccount2.OverdraftInterestRate = 5.0/100;
-    MyAccount2 += 2000;
-    MyAccount2 -= 3000;
-    MyAccount2.PayOverdraftInterest();
-    std::cout<<"Balance: "<< MyAccount2 <<std::endl;
-
-    SavingsAccount MyAccount3;
-    MyAccount3.InterestRate = 2.0/100;
-    MyAccount3 -= 100;
-    MyAccount3.CapitaliseIterest();
-    std::cout<<"Balance: "<< MyAccount3 <<std::endl;
-
-    MyAccount3 += 5000;
-    MyAccount3 -= 1000;
-    std::cout<<"Balance: "<< MyAccount3 <<std::endl;
+    MyAccount2.OverdraftLimit = 1000;    
 
     // vector<Account*> accounts = {&MyAccount1, &MyAccount2, &MyAccount3};
     // accounts.insert (&MyAccount4);
     
-    Account* myTab[] = {&MyAccount1, &MyAccount2, &MyAccount3}; //Jak hurtowo zwiększyć balans
+    Account* MyTab[] = {&MyAccount1, &MyAccount2, &MyAccount3, &MyAccount4};
 
-    for (int i = 0; i < sizeof(myTab)/sizeof(myTab[0]); i++)
+    for (int i = 0; i < sizeof(MyTab)/sizeof(MyTab[0]); i++)
     {
-        myTab[i] -> Balance += 3;
-        std::cout<<"Balance: "<< myTab[i] -> Balance <<std::endl;
+        MyTab[i] -> Set_Currency("PL");
+        OperationNumber++;        
+        ShowLastOperationRaport(MyTab[i]);
     }
+    ShowSeparationLine();
 
+    for (int i = 0; i < sizeof(MyTab)/sizeof(MyTab[0]); i++)
+    {
+        MyTab[i] -> Set_Currency("PLN");
+        OperationNumber++;
+        ShowLastOperationRaport(MyTab[i]);
+    }
+    ShowSeparationLine();
+
+    MyAccount4.Set_Currency("CHF");
+    OperationNumber++;
+    ShowLastOperationRaport(&MyAccount4);
+    ShowSeparationLine();
+
+
+    for (int i = 0; i < sizeof(MyTab)/sizeof(MyTab[0]); i++)
+    {
+        MyTab[i] -> Set_Balance(1000);
+        OperationNumber++;
+        ShowLastOperationRaport(MyTab[i]);
+    }
+    ShowSeparationLine();
+
+    for (int i = 0; i < sizeof(MyTab)/sizeof(MyTab[0]); i++)
+    {
+        MyTab[i] -> Withdraw(1001);    
+        OperationNumber++;
+        ShowLastOperationRaport(MyTab[i]);        
+    }    
+    ShowSeparationLine();
+
+    for (int i = 0; i < sizeof(MyTab)/sizeof(MyTab[0]); i++)
+    {
+        MyTab[i] -> Deposit(300); 
+        OperationNumber++;
+        ShowLastOperationRaport(MyTab[i]);        
+    }    
+    ShowSeparationLine();
+
+/*
+    for (Account *acc:MyTab)
+    {
+        acc-> Balance += 3;
+        std::cout<<"Balance: "<< acc -> Balance <<std::endl;     
+    }
+*/
     return 0;
 }
 
@@ -244,5 +449,51 @@ int main()
 *   2. Czy sensowne jest przerobienie typu enum Currencies_t na prostą klasę?
 *   3. Przeładowałem operator+=() i operator-=(). Nie bardzo rozumiem celowość przeciążania operator+() i operator-(). 
 *   4. Przeładowałem operator<<, ale są pytania ***
+*
+*   Zadania ze spotkania 20.09.2024  
+*       - Strzałki w diagramach klas UML a rodzaje relacji. Przyjrzeć się innym typom strzałek. Jakie typy relacji one reprezentują.
+*       - Zaimplementować przeładowanie operatorów "+" i "+=". Będą podobne do siebie. Czy można je jakoś uwspólnić
+*       - Nadpisywanie funkcji Withdraw() - przyjrzeć się. Która się wywoła, przy dostępie "hurtowym"? 
+*
+*   Pytania:
+*       - Są 4 klasy, które powinny dostarczyć implementrację funkcji Withdraw(). Jaka jest praktyka odnośnie miejsca gdzie umieszcza się 
+*           indywidualną wersję? Wszystkie abstrakcyjne wersje w jednym miejscu - np. tuż po klasie abstrakcyjnej? Czy może rozrzucone 
+*           po klasach szczegółowych - ja umieściłem je tuż po definicji klasy jako zawsze pierwszą implementowaną metodę danej klasy. Odp: Nie, za to podział na pliki (plik per klasa)
+*       - po co jest ""=0" Odp: oznacza, że klasa bazowa nie posiada implementacji danej metody, za to każdy potomek musi ją dostarczyć
+         (C++11 wprwowadza słówko override, aby z perspektywy klasy dziedziczącej było widać, ze po stronie klasy macieżystej jesy to metoda typu virtual)
+*   
+*   Zadania ze spotkania 04.10.2024:
+*       - podzielić na pliki
+*       - Przygotować dla treningu inne metody które wymagają wirtualizmu (Balance też virtual, może kapitalizacja)  
+*       - Enkapsulacja get/set schować pola jako prywatne lub protected i przygotować dla nich get-ery y set-ery
+*       - Zaimplementować przeładowanie operatorów "+" i "+=". Będą podobne do siebie. Czy można je jakoś uwspólnić? (zaległość z poprzedniego soptkania)
+* 
+*    Pomysły 
+*       - status operacji, stan przed i stan po - dużo kodu nadmiarowego
+*       - w takim podejściu getery nie mogą zwracać statusu
+*       - temat waluty jest źle wymyślony z logicznego punktu widzenia. 
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
 *
 *******************************************************************************/
